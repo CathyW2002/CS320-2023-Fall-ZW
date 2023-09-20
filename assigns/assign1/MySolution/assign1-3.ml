f3314c4ca9394575363bfa5415afdb72faadd4ee
CathyW2002 <zxwang12@bu.edu>
2023-09-19 21:08:31 -0400

    Add files via upload

---
+++ assigns/assign1/MySolution/assign1-3.ml
@@ -0,0 +1,40 @@
+(*
+assign1-3: 10 points
+A 3-letter sequence abc is 132-like
+if a < c < b holds. For instance, 123 is
+not 132-like; but 596 is 132-like.
+
+A string is 132-avoid if there is no subsequence
+abc in this string that is 132-like. Note that a
+subsequence of a string may contain NON-CONSEQUTIVE
+letters of the string.
+
+For instance, 123456789 is 132-avoid;
+For instance, 987654321 is 132-avoid;
+For instance, 123465789 is not 132-avoid since the
+subsequence 165 is 132-like.
+
+Please implement a function string_avoid_132 that
+checks if a given string is 132-avoid; the function
+returns true if and only if the given string is 132-
+avoid.
+
+fun string_avoid_132(cs: string): bool
+*)
+
+exception Found_132
+
+let string_avoid_132 (cs: string): bool =
+  let n = String.length cs in
+  try
+    for i = 0 to n - 1 do
+      for j = i + 1 to n - 1 do
+        for k = j + 1 to n - 1 do
+          if cs.[i] < cs.[k] && cs.[k] < cs.[j] then
+            raise Found_132
+        done
+      done
+    done;
+    true
+  with Found_132 -> false
+;;
