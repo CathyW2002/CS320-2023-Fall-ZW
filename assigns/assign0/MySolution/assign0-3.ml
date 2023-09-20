cf1a845ee4d09b6cc1f5fbf295f077aac167a28b
CathyW2002 <zxwang12@bu.edu>
2023-09-20 18:52:08 -0400

    Update assign0-3.ml

--- assigns/assign0/MySolution/assign0-3.ml
+++ assigns/assign0/MySolution/assign0-3.ml
@@ -5,11 +5,31 @@ integer to a string that represents the integer:
 fun int2str(i0: int): string
 *)
 
-let int2str (i0: int): string =
-  if i0 = 0 then "0" else
-  let rec loop n acc =
-      if n = 0 then acc
-      else loop (n / 10) (Char.escaped (Char.chr (48 + (n mod 10))) ^ acc)
+let chr = Char.chr
+let ord = Char.code
+let str(c0) = String.make 1 c0
+
+let int2str(i0: int): string =
+  (* Check if the number is negative *)
+  let is_negative = i0 < 0 in
+  
+  (* Work with the absolute value of the number *)
+  let num = if is_negative then (-i0) else i0 in
+
+  (* Convert a single digit to its character representation *)
+  let digit_to_char n = chr (ord '0' + n) in
+
+  (* Recursively extract digits and convert them to characters *)
+  let rec extract_digits n acc =
+    if n = 0 && acc = [] then [digit_to_char 0] (* handle case when i0 = 0 *)
+    else if n = 0 then acc
+    else extract_digits (n / 10) ((digit_to_char (n mod 10)) :: acc)
   in
-  if i0 < 0 then "-" ^ loop (-i0) "" else loop i0 ""
+
+  (* Construct the string from the list of character digits *)
+  let digits = extract_digits num [] in
+  let result = String.concat "" (List.map str digits) in
+
+  (* Add negative sign if the original number was negative *)
+  if is_negative then "-" ^ result else result
 ;;
