eb6f6dc13b66712abdbd4793e61768bc6c7d8c00
CathyW2002 <zxwang12@bu.edu>
2023-09-27 17:09:34 -0400

    Update assign2-4.ml

--- assigns/assign2/MySolution/OCaml/assign2-4.ml
+++ assigns/assign2/MySolution/OCaml/assign2-4.ml
@@ -1,5 +1,6 @@
 #use "./../../../classlib/OCaml/MyOcaml.ml";; 
 #use "./../../assign2.ml";;
+
 let string_sepjoin_list (sep: string)(xs: string list): string =
   let add_sep x = if x = "" then [] else [x; sep] in
   let with_sep = List.flatten (List.map add_sep xs) in
@@ -10,4 +11,26 @@ let string_sepjoin_list (sep: string)(xs: string list): string =
     String.sub result 0 (result_length - sep_length)
   else
     result
+;;
+
+let list_make_fwork f = 
+  let acc = ref [] in
+  f (fun x -> acc := x :: !acc);
+  List.rev !acc
+;;
+
+let list_foreach xs f = List.iter f xs;;
+
+let string_make_fwork f =
+  let buffer = Buffer.create 16 in
+  f (fun c -> Buffer.add_char buffer c);
+  Buffer.contents buffer
+;;
+
+let string_foreach s f = String.iter f s;;
+
+let string_concat_list(css: string list): string =
+  string_make_fwork(
+    fun work -> list_foreach css (fun cs -> string_foreach cs work)
+  )
 ;;
