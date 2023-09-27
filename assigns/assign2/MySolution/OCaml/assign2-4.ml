f4e66d8d9dbdb5f30735b7fa52218a03d5447d58
CathyW2002 <zxwang12@bu.edu>
2023-09-27 18:04:44 -0400

    Update assign2-4.ml

--- assigns/assign2/MySolution/OCaml/assign2-4.ml
+++ assigns/assign2/MySolution/OCaml/assign2-4.ml
@@ -1,14 +1,9 @@
 #use "./../../../classlib/OCaml/MyOCaml.ml";; 
 #use "./../../assign2.ml";;
-
 let string_sepjoin_list (sep: string)(xs: string list): string =
-  let add_sep x = if x = "" then [] else [x; sep] in
-  let with_sep = List.flatten (List.map add_sep xs) in
-  let result = string_concat_list with_sep in
-  let result_length = String.length result in
-  let sep_length = String.length sep in
-  if result_length >= sep_length then
-    String.sub result 0 (result_length - sep_length)
-  else
-    result
+  let combine acc s = 
+    if acc = "" then s
+    else acc ^ sep ^ s
+  in
+  List.fold_left combine "" xs
 ;;
