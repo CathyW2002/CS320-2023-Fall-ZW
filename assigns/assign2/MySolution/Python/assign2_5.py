8a5443902997ff77b382ea646e91c3e11639cbe2
CathyW2002 <zxwang12@bu.edu>
2023-09-27 16:33:28 -0400

    Update assign2_5.py

--- assigns/assign2/MySolution/Python/assign2_5.py
+++ assigns/assign2/MySolution/Python/assign2_5.py
@@ -1,15 +1,19 @@
-def fnlist_reverse(xs):
-    res = fnlist_nil()
-    for x in xs:
-        res = fnlist_cons(x, res.get_cons2())
-    return res
-
 def fnlist_make_fwork(fwork):
     res = fnlist_nil()
-
+    
     def work(x0):
         nonlocal res
         res = fnlist_cons(x0, res)
-
+    
     fwork(work)
-    return fnlist_reverse(res)
+    
+    # create an empty fnlist to hold the reversed list
+    reversed_res = fnlist_nil()
+    temp = res  # point a temporary variable at res
+
+    # traverse through the original list and prepend each item to the reversed list
+    while temp.ctag != 0:
+        reversed_res = fnlist_cons(temp.cons1, reversed_res)
+        temp = temp.cons2  # move to the next item in the original list
+    
+    return reversed_res
