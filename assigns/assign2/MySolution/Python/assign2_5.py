7c7c0c80d7854b1b9495ad5421a2cb24d6c7ac87
CathyW2002 <zxwang12@bu.edu>
2023-09-27 16:44:15 -0400

    Update assign2_5.py

--- assigns/assign2/MySolution/Python/assign2_5.py
+++ assigns/assign2/MySolution/Python/assign2_5.py
@@ -1,9 +1,16 @@
+def fnlist_reverse(xs):
+    res = fnlist_nil()
+    iterator = fnlist_iter(xs)
+    for x in iterator:
+        res = fnlist_cons(x, res)
+    return res
+
 def fnlist_make_fwork(fwork):
     res = fnlist_nil()
-    
+
     def work(x0):
         nonlocal res
         res = fnlist_cons(x0, res)
-    
+
     fwork(work)
-    return res
+    return fnlist_reverse(res)
