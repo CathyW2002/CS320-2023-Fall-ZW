e4aa4cbc8dac498edcc5e0b97bbc793f08bc1e73
CathyW2002 <zxwang12@bu.edu>
2023-09-27 17:14:29 -0400

    Update assign2-4.ml

--- assigns/assign2/MySolution/OCaml/assign2-4.ml
+++ assigns/assign2/MySolution/OCaml/assign2-4.ml
@@ -12,25 +12,3 @@ let string_sepjoin_list (sep: string)(xs: string list): string =
   else
     result
 ;;
-
-let list_make_fwork f = 
-  let acc = ref [] in
-  f (fun x -> acc := x :: !acc);
-  List.rev !acc
-;;
-
-let list_foreach xs f = List.iter f xs;;
-
-let string_make_fwork f =
-  let buffer = Buffer.create 16 in
-  f (fun c -> Buffer.add_char buffer c);
-  Buffer.contents buffer
-;;
-
-let string_foreach s f = String.iter f s;;
-
-let string_concat_list(css: string list): string =
-  string_make_fwork(
-    fun work -> list_foreach css (fun cs -> string_foreach cs work)
-  )
-;;
