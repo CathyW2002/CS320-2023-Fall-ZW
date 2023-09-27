3f6821761dc7f87eb8db6b6e02112e50dd1c3b36
CathyW2002 <zxwang12@bu.edu>
2023-09-27 16:04:12 -0400

    Update assign2-4.ml

--- assigns/assign2/MySolution/OCaml/assign2-4.ml
+++ assigns/assign2/MySolution/OCaml/assign2-4.ml
@@ -1,6 +1,22 @@
 #use "./../../../classlib/OCaml/MyOcaml.ml";; 
 #use "./../../assign2.ml";;
 
+let list_make_fwork f = 
+  let acc = ref [] in
+  f (fun x -> acc := x :: !acc);
+  List.rev !acc
+;;
+
+let list_foreach xs f = List.iter f xs;;
+
+let string_make_fwork f =
+  let buffer = Buffer.create 16 in
+  f (fun c -> Buffer.add_char buffer c);
+  Buffer.contents buffer
+;;
+
+let string_foreach s f = String.iter f s;;
+
 let string_sepjoin_list (sep: string)(xs: string list): string =
   let with_sep = List.map (fun x -> [x; sep]) xs in
   let flattened = list_concat with_sep in
