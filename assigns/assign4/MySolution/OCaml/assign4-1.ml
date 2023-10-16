606f8a1772f8c2bc75480c76dda34f3a7eda6931
CathyW2002 <zxwang12@bu.edu>
2023-10-16 15:54:46 -0400

    Update assign4-1.ml

--- assigns/assign4/MySolution/OCaml/assign4-1.ml
+++ assigns/assign4/MySolution/OCaml/assign4-1.ml
@@ -1,18 +1,20 @@
 #use "./../../../../classlib/OCaml/MyOCaml.ml";;
 
-type 'a strcon = Cons of 'a * (unit -> 'a strcon);;
+(* Define the 'strcon' type once, and do not redefine it in this session. *)
+type 'a strcon = Cons of 'a * (unit -> 'a strcon)
+;;
 
-let the_ln2_stream () =
-  let rec next i =
-    let value = if i mod 2 = 0 then -.1.0 /. float_of_int i else 1.0 /. float_of_int i in
-    Cons (value, fun () -> next (i + 1))  (* Construct the stream element *)
-  in
-  next 1  (* Start with the first element *);;
+(* Helper function to generate the next element of the ln(2) series. *)
+let rec next_ln2_element i sum =
+  let new_sum = if i mod 2 = 0 then sum -. 1.0 /. float_of_int i else sum +. 1.0 /. float_of_int i in
+  Cons(new_sum, fun () -> next_ln2_element (i + 1) new_sum)
+;;
 
-(* Function to generate the partial sums of the series *)
-let rec partial_sums n stream =
-  let rec helper count acc (Cons (head, tail)) =
-    if count <= 0 then acc
-    else helper (count - 1) (acc +. head) (tail ())
-  in
-  helper n 0.0 stream;;
+(* The stream representing the partial sums of the ln(2) series. *)
+let the_ln2_stream () : float strcon = next_ln2_element 1 1.0 ;;
+
+(* Usage: function to retrieve the n-th element of a stream *)
+let rec nth_stream_element (Cons (head, tail)) n =
+  if n = 1 then head
+  else nth_stream_element (tail ()) (n - 1)
+;;
