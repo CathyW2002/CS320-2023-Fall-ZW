d73cd3d337f2c182015accedf69c0394ac87f5ff
CathyW2002 <zxwang12@bu.edu>
2023-10-16 15:57:52 -0400

    Update assign4-1.ml

--- assigns/assign4/MySolution/OCaml/assign4-1.ml
+++ assigns/assign4/MySolution/OCaml/assign4-1.ml
@@ -1,20 +1,15 @@
 #use "./../../../../classlib/OCaml/MyOCaml.ml";;
 
-(* Define the 'strcon' type once, and do not redefine it in this session. *)
-type 'a strcon = Cons of 'a * (unit -> 'a strcon)
-;;
+(* Definitions of the custom stream types and exceptions *)
+exception Empty;;
 
-(* Helper function to generate the next element of the ln(2) series. *)
-let rec next_ln2_element i sum =
-  let new_sum = if i mod 2 = 0 then sum -. 1.0 /. float_of_int i else sum +. 1.0 /. float_of_int i in
-  Cons(new_sum, fun () -> next_ln2_element (i + 1) new_sum)
-;;
+type 'a strcon = unit -> 'a strcell;;
+and 'a strcell = StrNil | StrCons of 'a * 'a strcon;;
 
-(* The stream representing the partial sums of the ln(2) series. *)
-let the_ln2_stream () : float strcon = next_ln2_element 1 1.0 ;;
-
-(* Usage: function to retrieve the n-th element of a stream *)
-let rec nth_stream_element (Cons (head, tail)) n =
-  if n = 1 then head
-  else nth_stream_element (tail ()) (n - 1)
-;;
+(* Implementing the stream for the ln(2) series *)
+let the_ln2_stream : float strcon =
+  let rec next i sum =
+    let new_sum = if i mod 2 = 0 then sum -. (1. /. float_of_int i) else sum +. (1. /. float_of_int i) in
+    fun () -> StrCons(new_sum, next (i + 1) new_sum)
+  in
+  next 1 1.0;;
