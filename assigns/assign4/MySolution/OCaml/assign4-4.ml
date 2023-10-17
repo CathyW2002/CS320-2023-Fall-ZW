cbf7702213088840a0fcb45c6289186d7a87b435
CathyW2002 <zxwang12@bu.edu>
2023-10-17 16:10:39 -0400

    Update assign4-4.ml

--- assigns/assign4/MySolution/OCaml/assign4-4.ml
+++ assigns/assign4/MySolution/OCaml/assign4-4.ml
@@ -1,42 +1,52 @@
 #use "./../../../../classlib/OCaml/MyOCaml.ml";;
 
-type 'a strcon =
-  | StrNil
-  | StrCons of 'a * (unit -> 'a strcon);;
+type 'a stream = StrNil | StrCons of 'a * (unit -> 'a stream);;
 
-type 'a stream = unit -> 'a strcon;;
-
-let rec insert_everywhere x lst =
-  let rec aux prev acc = function
-    | [] -> list_revapp acc [[x]]
-    | (hd :: tl) as l -> 
-      let new_list = list_revapp prev (x :: l) in
-      aux (hd :: prev) (new_list :: acc) tl
-  in
-  aux [] [] lst
-;;
-
-let rec permutations lst =
-  match lst with
-  | [] -> [[]]
-  | hd :: tl ->
-    let perm_tl = permutations tl in
-    let rec aux acc = function
-      | [] -> acc
-      | l :: ls ->
-        let new_lists = insert_everywhere hd l in
-        aux (list_revapp new_lists acc) ls
+let list_permute (xs : 'a list) : 'a list stream =
+  let rec extract_at idx lst =
+    let rec extract acc i = function
+      | [] -> None  
+      | h :: t as l -> if i = idx then Some (h, list_revapp acc t) else extract (h :: acc) (i + 1) l
     in
-    aux [] perm_tl
-;;
+    extract [] 0 lst
+  in
 
-let rec list_to_stream lst () =
-  match lst with
-  | [] -> StrNil
-  | hd :: tl -> StrCons (hd, list_to_stream tl)
-;;
+  let rec interleave x lst =
+    let rec aux pre suf acc = match suf with
+      | [] -> (List_reverse (x :: pre)) :: acc
+      | hd::tl ->
+        let new_pre = list_revapp tl (x :: pre) in
+        aux (hd::pre) tl (new_pre :: acc)
+    in
+    aux [] lst []
+  in
 
-let list_permute (xs: 'a list) : 'a list stream =
-  let perms = permutations xs in
-  list_to_stream perms
+  let rec permute lst =
+    match lst with
+    | [] -> StrCons ([], fun () -> StrNil)
+    | _  ->
+      let n = foreach_to_length(list_foreach)(lst) in
+      let rec loop i =
+        if i >= n then StrNil
+        else
+          match extract_at i lst with
+          | None -> StrNil 
+          | Some (element, rest) ->
+            let subperms_stream = permute rest in
+            let rec distribute perms_stream =
+              match perms_stream with
+              | StrNil -> loop (i + 1)
+              | StrCons (perm, get_next_stream) ->
+                let interleaved = interleave element perm in
+                let rec concat_interleaved il =
+                  match il with
+                  | [] -> distribute (get_next_stream ())
+                  | h :: t -> StrCons (h, fun () -> concat_interleaved t)
+                in
+                concat_interleaved interleaved
+            in
+            distribute subperms_stream
+      in loop 0
+  in
+  permute xs
 ;;
