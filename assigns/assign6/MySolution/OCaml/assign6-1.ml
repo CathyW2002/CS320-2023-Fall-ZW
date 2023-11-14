f67b506c9b9cabc0e24826a627270b46a9bb28f7
CathyW2002 <zxwang12@bu.edu>
2023-11-13 20:43:31 -0500

    Update assign6-1.ml

--- assigns/assign6/MySolution/OCaml/assign6-1.ml
+++ assigns/assign6/MySolution/OCaml/assign6-1.ml
@@ -98,7 +98,7 @@ sexpr_parse "((mul 1 2)" = None
 
 (* ****** ****** *)
 
-
+(*
 Grammar (<expr> is the start symbol)
 
 <digit> ::= 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
@@ -107,7 +107,7 @@ Grammar (<expr> is the start symbol)
 <expr>  ::= <num>
           | (add <exprs> )
           | (mul <exprs> )
-
+*)
 type sexpr =
   | SInt of int        (* 1, 2, 3, 4 ...  *)
   | SAdd of sexpr list (* (add e1 e2 ...) *)
